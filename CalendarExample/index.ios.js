e340b361038e31c8a5772e8d7ae2d372d1c4f580
Christopher <hello@blick-labs.com>
2015-08-07 10:35:38 -0700

    Add new demo gif. Typo fixes

--- CalendarExample/index.ios.js
+++ CalendarExample/index.ios.js
@@ -27,7 +27,6 @@ var CalendarExample = React.createClass({
           eventDates={['2015-07-03', '2015-07-05', '2015-07-10', '2015-07-15', '2015-07-20', '2015-07-25', '2015-07-28', '2015-07-30']}
           scrollEnabled={true}
           showControls={true}
-          selectedDate={'2015-08-15'}
           dayHeadings={customDayHeadings}
           titleFormat={'MMMM YYYY'}
           prevButtonText={'Prev'}
