5fe0863a480f0436af4838c7523a94c5306238e4
Rajesh Sivakumar <rajeshs@LT-MAC46.local>
2017-01-18 09:23:20 -0800

    Fixed the issue with the calendar not rendering properly when inside a Collapsible component

--- components/Calendar.js
+++ components/Calendar.js
@@ -294,7 +294,7 @@ export default class Calendar extends Component {
             horizontal
             scrollEnabled
             pagingEnabled
-            removeClippedSubviews
+            removeClippedSubviews={false}
             scrollEventThrottle={1000}
             showsHorizontalScrollIndicator={false}
             automaticallyAdjustContentInsets
