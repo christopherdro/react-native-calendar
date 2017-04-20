d4ceb89cb8159d658419ab75f7b1748ceaaae83f
Ivan <cherniv@gmail.com>
2017-04-20 13:01:30 +0300

    Changing default width calculation to percentage

--- components/styles.js
+++ components/styles.js
@@ -7,7 +7,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#f7f7f7',
   },
   monthContainer: {
-    width: DEVICE_WIDTH,
+    width: '100%',
   },
   calendarControls: {
     flexDirection: 'row',
@@ -48,13 +48,13 @@ const styles = StyleSheet.create({
   dayButton: {
     alignItems: 'center',
     padding: 5,
-    width: DEVICE_WIDTH / 7,
+    width: 14.2 + '%',
     borderTopWidth: 1,
     borderTopColor: '#e9e9e9',
   },
   dayButtonFiller: {
     padding: 5,
-    width: DEVICE_WIDTH / 7,
+    width: 14.2 + '%',
   },
   day: {
     fontSize: 16,
